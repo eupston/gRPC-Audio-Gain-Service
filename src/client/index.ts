cac7a2a1fdbbe9ede165394a4e93c43493d6a1e9
eupston <eupston130@hotmail.com>
2021-03-16 23:59:05 +1300

    updated with initial mic streaming from client to server

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,21 +1,33 @@
 import {AudioSample} from '../proto/audiostreamer_pb'
 import {client} from './utils'
+import {AudioIO, SampleFormat16Bit} from 'naudiodon'
 
-async function run() {
-    let stream = client.audioStream()
+
+async function startAudioStream() {
+
+    let micstream = AudioIO({
+        inOptions: {
+            channelCount: 2,
+            sampleFormat: SampleFormat16Bit,
+            sampleRate: 44100,
+            deviceId: -1, // Use -1 or omit the deviceId to select the default device
+            closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
+        }
+    });
     let sample = new AudioSample()
-    sample.setData(new Uint8Array([0x0102]))
-    sample.setTimestamp(new Date() + " from client")
-    stream.write(sample)
-    stream.end()
+    let stream = client.audioStream()
+
+    micstream.on('data', async (data) => {
+        sample.setData(data)
+        sample.setTimestamp(new Date() + " from client")
+        stream.write(sample)
+    })
     stream.on('data', (serversample) => {
-        console.log(serversample.getTimestamp())
         console.log(serversample.getData())
     })
-    stream.on('end', function(event) {
-        stream.end();
-        process.exit();
-    });
+
+    //Start streaming
+    micstream.start();
 }
 
-run()
+startAudioStream()
